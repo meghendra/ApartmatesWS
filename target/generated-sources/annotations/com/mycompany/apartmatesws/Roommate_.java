e7a3454b8d256effcc44fe84274a03ea90b4173e
AndresPico <apicochavez@mail.usf.edu>
2016-12-15 04:46:29 -0500

    no message

--- target/generated-sources/annotations/com/mycompany/apartmatesws/Roommate_.java
+++ target/generated-sources/annotations/com/mycompany/apartmatesws/Roommate_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-13T18:03:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-15T04:18:17")
 @StaticMetamodel(Roommate.class)
 public class Roommate_ { 
 
